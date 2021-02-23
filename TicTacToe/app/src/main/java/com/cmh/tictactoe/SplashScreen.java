8c22b54295d09e72e4c2fa09a26dd68e88414a4a
manyl <manyl-telmat@hotmail.com>
2021-02-22 21:23:24 -0500

    splash screen

--- TicTacToe/app/src/main/java/com/cmh/tictactoe/SplashScreen.java
+++ TicTacToe/app/src/main/java/com/cmh/tictactoe/SplashScreen.java
@@ -1,48 +1,54 @@
 package com.cmh.tictactoe;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
-import androidx.appcompat.app.AppCompatActivity;
+import android.widget.TextView;
 
+import com.cmh.tictactoe.R;
 
 public class SplashScreen extends AppCompatActivity {
-
-    ImageView backgroundImage;
-    Animation slideAnimation;
-    Integer SPLASH_SCREEN_TIME_OUT = 4000;
-
+    private static int COUNTER= 4000;//compteur splash screen en ms
+    Animation top_anim,bottom_anim;// fichiers d'animaion
+    ImageView image; //logo
+    TextView logo; // texte klk
+    MediaPlayer welcome_music;// music du splash screen
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // masquer la status bar
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
+        //music
+        welcome_music= MediaPlayer.create(this,R.raw.welcome);
+        //animations
+        top_anim= AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottom_anim= AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
 
-        // This is used to hide the status bar and make
-        // the splash screen as a full screen activity.
-        getWindow().setFlags(
-                WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN
-        );
+        image=findViewById(R.id.imageView);
+        logo=findViewById(R.id.textView);
 
-        // HERE WE ARE TAKING THE REFERENCE OF OUR IMAGE
-        // SO THAT WE CAN PERFORM ANIMATION USING THAT IMAGE
-        backgroundImage = (ImageView) findViewById(R.id.SplashScreenImage);
-        slideAnimation = AnimationUtils.loadAnimation(this, R.anim.splash_animation);
-        backgroundImage.startAnimation(slideAnimation);
+        welcome_music.start();
+        image.setAnimation(top_anim);
+        logo.setAnimation(bottom_anim);
 
-        // we used the postDelayed(Runnable, time) method
-        // to send a message with a delayed time.
-        Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
+
+        // apres 4000ms on stoope la music et on passe au meu principale
+        new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                Intent intent =new Intent(SplashScreen.this,MainActivity.class);
                 startActivity(intent);
+                welcome_music.release();
+                finish();//on termine lactivite splash screen (si il u a precedent lapp se ferme)
             }
-        }, SPLASH_SCREEN_TIME_OUT); // 4000 is the delayed time in milliseconds.
+        },COUNTER);
     }
 }
